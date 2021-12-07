a6f0a2da819e7d6fa41db1ed5b1c8e11dd39c9c1
JacobG03 <jaqobek1995@gmail.com>
2021-12-07 21:53:23 +0100

    gun & bullet classes added

--- shooter/sketch.js
+++ shooter/sketch.js
@@ -1,20 +1,24 @@
 // Global
-const FACE_UP = 0
-const FACE_RIGHT = 1
-const FACE_DOWN = 2
-const FACE_LEFT = 3
+const UP = 0
+const RIGHT = 1
+const DOWN = 2
+const LEFT = 3
 
 const Config = {
 	Player: {
-		width: 64,
-		speed: 2,
+		width: 32,
+		speed: 4,
 		sprint: 2
+	},
+	Gun: {
+		fireRate: 50,
+		range: 2000
 	},
 	Bullet: {
 		color: 'rgba(0, 0, 200, 0.5)',
-		height: 20,
-		width: 5,
-		speed: 10,
+		height: 60,
+		width: 10,
+		speed: 6,
 	}
 }
 
@@ -25,14 +29,12 @@ const User = {
 }
 
 let avatar;
-let gameMap
-let players = []
-let bullets = []
+let game;
 
 const gameInit = () => {
 	let player = new Player(width / 2, height / 2, User)
-	players.push(player)
-	gameMap = new GameMap(0, 0, 800, 800)
+	game = new Game()
+	game.players.push(player)
 }
 
 function preload() {
@@ -42,33 +44,88 @@ function preload() {
 function setup() {
 	createCanvas(800, 800);
 	gameInit()
+	frameRate(60)
 }
 
 function draw() {
 	background(160)
 	// handle bullets
-	for (let i = 0; i < bullets.length; i++) {
-		let bullet = bullets[i]
+	for (let i = 0; i < game.bullets.length; i++) {
+		let bullet = game.bullets[i]
 		bullet.fly()
 		bullet.display()
 	}
 	// handle players
-	for (let i = 0; i < players.length; i++) {
-		let player = players[i]
+	for (let i = 0; i < game.players.length; i++) {
+		let player = game.players[i]
 		if (keyIsPressed) {
 			player.movement()
+			player.attack()
 		}
 		player.display()
 	}
-	// validate game
-	gameMap.validatePlayersBoundries(players)
+
+	game.checkForCollisions()
+}
+
+class Game {
+	constructor() {
+		this.players = []
+		this.bullets = []
+	}
+
+	checkForCollisions() {
+		this.validateBulletBoundries()
+		this.validatePlayersBoundries()
+	}
+
+	validatePlayersBoundries() {
+		for (let i = 0; i < this.players.length; i++) {
+			let player = this.players[i]
+			// check horizontally
+			// top
+			if (player.x < 0) {
+				player.x = width - player.width
+				// bottom
+			} else if (player.x + player.width > width) {
+				player.x = 0
+			// check vertically
+				// left
+			} else if (player.y < 0) {
+				player.y = height - player.width
+				// right
+			} else if (player.y + player.width > height) {
+				player.y = 1
+			}
+		}
+	}
+
+	validateBulletBoundries() {
+		for (let i = 0; i < this.bullets.length; i++) {
+			let bullet = this.bullets[i]
+			// check horizontally
+			// top
+			if (bullet.x < 0) {
+				bullet.x = width - bullet.width
+				// bottom
+			} else if (bullet.x + bullet.width > width) {
+				bullet.x = 0
+			// check vertically
+				// left
+			} else if (bullet.y < 0) {
+				bullet.y = height - bullet.width
+				// right
+			} else if (bullet.y + bullet.width > height) {
+				bullet.y = 1
+			}
+		}
+	}
 }
 
 class Player {
 	constructor(x, y, user) {
 		this.x = x
 		this.y = y
-		this.facing = 2 // top = 0, right = 1, bottom = 2, left = 3
 		this.speed = Config.Player.speed
 		this.walk_speed = this.speed
 		this.sprint_speed = this.speed * Config.Player.sprint
@@ -83,20 +140,16 @@ class Player {
 		// up
 		if (keyIsDown(87)) {
 			this.y -= this.walk_speed
-			this.facing = FACE_UP
 			// down
 		} else if(keyIsDown(83)) {
 			this.y += this.walk_speed
-			this.facing = FACE_DOWN
 		}
 		// left
 		if (keyIsDown(65)) {
 			this.x -= this.walk_speed
-			this.facing = FACE_LEFT
 		} else if (keyIsDown(68)) {
 			// right
 			this.x += this.walk_speed
-			this.facing = FACE_RIGHT
 		}
 		// sprint
 		if (keyIsDown(16)) {
@@ -104,10 +157,26 @@ class Player {
 		} else {
 			this.walk_speed = this.speed
 		}
-		// Power
-		if (keyIsDown(32)) {
+	}
+
+	attack() {
+		// if arrow key pressed
+		if (keyIsDown(37)) {
 			let gun = this.weapons[this.weapon];
-			gun.shoot()
+			let direction = LEFT
+			gun.shoot(direction)
+		} else if(keyIsDown(38)) {
+			let gun = this.weapons[this.weapon];
+			let direction = UP
+			gun.shoot(direction)
+		} else if(keyIsDown(39)) {
+			let gun = this.weapons[this.weapon];
+			let direction = RIGHT
+			gun.shoot(direction)
+		} else if(keyIsDown(40)) {
+			let gun = this.weapons[this.weapon];
+			let direction = DOWN
+			gun.shoot(direction)
 		} 
 	}
 
@@ -120,13 +189,13 @@ class Gun {
 	constructor(user) {
 		this.user = user
 		this.canShoot = true
-		this.fireRate = 400 // can shoot every 400ms
+		this.fireRate = Config.Gun.fireRate // can shoot every 400ms
 	}
 
-	shoot() {
+	shoot(direction) {
 		if (this.canShoot) {
-			let bullet = new Bullet(this.user)
-			bullets.push(bullet)
+			let bullet = new Bullet(direction, this.user)
+			game.bullets.push(bullet)
 			this.canShoot = false
 			setTimeout(() => {
 				this.canShoot = true
@@ -136,75 +205,46 @@ class Gun {
 }
 
 class Bullet {
-	constructor(user) {
+	constructor(direction, user) {
 		this.x = user.x + user.width / 2
 		this.y = user.y + user.width / 2
+		this.direction = direction
 		this.width = Config.Bullet.width
 		this.height = Config.Bullet.height
-		this.direction = user.facing
 		this.speed = Config.Bullet.speed
+		this.distance = 0
 		this.user = user
 		this.color = Config.Bullet.color
 	}
 
 	fly() {
-		if (this.direction === FACE_UP) {
+		if (this.direction === UP) {
 			this.y -= this.speed
-		} else if (this.direction === FACE_DOWN) {
+		} else if (this.direction === DOWN) {
 			this.y += this.speed
-		} else if (this.direction === FACE_RIGHT) {
+		} else if (this.direction === RIGHT) {
 			this.x += this.speed
-		} else if (this.direction === FACE_LEFT) {
+		} else if (this.direction === LEFT) {
 			this.x -= this.speed
+		}
+		this.distance += this.speed
+
+		if (this.distance > Config.Gun.range) {
+			this.remove()
 		}
 	}
 
 	remove() {
-
+		game.bullets = game.bullets.filter(bullet => bullet !== this)
 	}
 
 	display() {
 		fill(this.color)
 		rectMode(CENTER)
-		if (this.direction === FACE_UP || this.direction === FACE_DOWN) {
-			print('up')
+		if (this.direction === UP || this.direction === DOWN) {
 			rect(this.x, this.y, this.width, this.height)
 		} else {
 			rect(this.x, this.y, this.height, this.width)
 		}
-	}
-}
-
-class GameMap {
-	constructor(x, y, width, height) {
-		this.x = x
-		this.y = y
-		this.width = width
-		this.height = height
-	}
-
-	validatePlayersBoundries(players) {
-		for (let i = 0; i < players.length; i++) {
-			let player = players[i]
-			// check horizontally
-			// top
-			if (player.x < 0) {
-				player.x = this.width - player.width
-				// bottom
-			} else if (player.x + player.width > this.width) {
-				player.x = 0
-			// check vertically
-				// left
-			} else if (player.y < 0) {
-				player.y = this.height - player.width
-				// right
-			} else if (player.y + player.width > this.height) {
-				player.y = 1
-			}
-		}
-	}
-
-	validateBulletBoundries(bullets) {
-
 	}
 }
